fc3f9cd3a92fda2eb5ffa9542642c2312114ef1b
bronze-wick <bronzewick.llc@gmail.com>
2017-09-05 20:35:47 -0400

    No setGravity method

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-//test
+//GeoQuiz2.0.7
 
 public class QuizActivity extends AppCompatActivity {
 
