3fdbd2c36a898f6ab0f1abef2e7f49d1f3f8ad4e
bronze-wick <bronzewick.llc@gmail.com>
2017-09-27 16:00:07 -0400

    No setGravity method

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,14 +2,18 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-//GeoQuiz2.1.0 Pushed to Master 09/19/17
+//GeoQuiz3.0.0
 
 public class QuizActivity extends AppCompatActivity {
+    //create tag in class quizactivity for log data
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     //member field variable declaration.
     private Button mTrueButton;
@@ -35,7 +39,12 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+        //Listing 3.7  Checking bundle in onCreate(Bundle)
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         //Linking of declared variable to view string id
         mTrueButton = (Button) findViewById(R.id.true_button);
@@ -78,6 +87,44 @@ public class QuizActivity extends AppCompatActivity {
 
         updateQuestion();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
